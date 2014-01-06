49b1ac56f71a73fe5c9435f7475719212401e1d9
mheinric <marc.heinrich@free.fr>
2014-01-06 11:30:51 +0100

    fix compile error

--- src/pg/main/Test.java
+++ src/pg/main/Test.java
@@ -7,15 +7,11 @@ import javax.xml.transform.TransformerException;
 
 import org.xml.sax.SAXException;
 
-import ORG.oclc.oai.harvester2.verb.GetRecord;
-
 
 
 public class Test {
 	
-	public static void test() throws IOException, ParserConfigurationException, SAXException, TransformerException{
-		GetRecord ident = new GetRecord("http://citeseerx.ist.psu.edu/oai2","oai:CiteSeerX.psu:10.1.1.1.2596", "oai_dc") ;
-		System.out.println(ident.toString()) ;
+	public static void test() {
 	}
 
 }
